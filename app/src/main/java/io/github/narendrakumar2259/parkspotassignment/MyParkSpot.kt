13cf8c78186e79e9d70d3fda9b11c03736f80c57
Ilavarapu Narendra Kumar <ink@qclairvoyance.in>
2026-07-11 21:53:08 +0530

    Add dependencies and application class

---
+++ app/src/main/java/io/github/narendrakumar2259/parkspotassignment/MyParkSpot.kt
@@ -0,0 +1,7 @@
+package io.github.narendrakumar2259.parkspotassignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyParkSpot: Application()
